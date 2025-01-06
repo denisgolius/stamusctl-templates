ff43f534d1367feaa55c8a225c60811ab464672e
lanath <valentin@viviersoft.com>
2025-01-06 15:47:29 +0100

    fix(tests): fix test template prerun script

--- bin/tests/pre-run/main.go
+++ bin/tests/pre-run/main.go
@@ -3,27 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	values := make(map[string]interface{})
-
-	yamlFile, err := os.ReadFile("./tmp/values.yaml")
+	if len(os.Args) != 3 {
+		fmt.Println("Missing config argument")
+		os.Exit(1)
+	}
+	// Read values
+	path := os.Args[2]
+	yamlFile, err := os.ReadFile(filepath.Join(path, "values.yaml"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading config file: %v", err)
 		os.Exit(1)
 	}
+	// Unmarshal values
+	values := make(map[string]interface{})
 	err = yaml.Unmarshal(yamlFile, values)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unmarshal: %v", err)
-		os.Exit(1)
 	}
 	data, err := yaml.Marshal(values)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Marshal: %v", err)
-		os.Exit(1)
 	}
 	fmt.Println(string(data))
 }
