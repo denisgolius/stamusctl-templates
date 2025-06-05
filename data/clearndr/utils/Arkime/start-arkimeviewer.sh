56a865e5865d42c7c4fbb00b7b33a693d26c87eb
lanath <valentin@viviersoft.com>
2025-06-05 10:45:38 +0200

    fix(clearndr): using opensearch 2.18 fixes issue with arkime
    Ref: #9

--- data/clearndr/utils/Arkime/start-arkimeviewer.sh
+++ data/clearndr/utils/Arkime/start-arkimeviewer.sh
@@ -20,7 +20,7 @@ done
 export ARKIME_ELASTICSEARCH="http://"$ES_HOST":"$ES_PORT
 
 if [ ! -f $ARKIMEDIR/etc/.initialized ]; then
-    echo "Initializing ES database..."
+    echo "Initializing $ARKIME_ELASTICSEARCH database..."
     echo INIT | $ARKIMEDIR/db/db.pl $ARKIME_ELASTICSEARCH init
     $ARKIMEDIR/bin/arkime_add_user.sh $ARKIME_ADMIN_USERNAME "SELKS Admin User" $ARKIME_ADMIN_PASSWORD --admin
     $ARKIMEDIR/bin/arkime_add_user.sh moloch moloch moloch --admin --webauth
@@ -42,15 +42,13 @@ else
 fi
 
 echo "Starting Arkime capture in the background..."
+mkdir -p $ARKIMEDIR/logs
 exec $ARKIMEDIR/bin/capture -m -s -R /suricata-logs/fpc/ >> $ARKIMEDIR/logs/capture.log 2>&1 &
-
 echo "Look at log files for errors"
-echo "  /data/logs/viewer.log"
-echo "  /data/logs/capture.log"
-echo "Visit http://127.0.0.1:8005 with your favorite browser."
-echo "  user: $ARKIME_ADMIN_USERNAME"
-echo "  password: $ARKIME_ADMIN_PASSWORD"
+echo "  $ARKIMEDIR/logs/capture.log"
+
+echo "user: $ARKIME_ADMIN_USERNAME"
 
 echo "Launch viewer..."
 cd $ARKIMEDIR/viewer
-$ARKIMEDIR/bin/node $ARKIMEDIR/viewer/viewer.js >> $ARKIMEDIR/logs/viewer.log 2>&1
+$ARKIMEDIR/bin/node $ARKIMEDIR/viewer/viewer.js
